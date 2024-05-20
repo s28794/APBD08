49ab74bf9c1ae93b48914290ade49d7c9c2618be
s28794 <s28794@pjwstk.edu.pl>
2024-05-20 21:24:51 +0200

    1m

---
+++ LinqTasks/LinqTutorials/LinqTasks/Extensions/CustomExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using LinqTasks.Models;
+
+namespace LinqTasks.Extensions;
+
+public static class CustomExtensionMethods
+{
+    //Put your extension methods here
+    public static IEnumerable<Emp> GetEmpsWithSubordinates(this IEnumerable<Emp> emps)
+    {
+        return emps.Where(emp => emps.Any(e => e.Mgr != null && e.Mgr.Empno == emp.Empno))
+            .OrderBy(emp => emp.Ename)
+            .ThenByDescending(emp => emp.Salary);
+    }
+}
---
+++ LinqTasks/LinqTutorials/LinqTasks/Tasks.cs
@@ -0,0 +1,160 @@
+﻿using LinqTasks.Extensions;
+using LinqTasks.Models;
+
+namespace LinqTasks;
+
+public static partial class Tasks
+{
+    public static IEnumerable<Emp> Emps { get; set; }
+    public static IEnumerable<Dept> Depts { get; set; }
+
+    static Tasks()
+    {
+        Depts = LoadDepts();
+        Emps = LoadEmps();
+    }
+
+    /// <summary>
+    ///     SELECT * FROM Emps WHERE Job = "Backend programmer";
+    /// </summary>
+    public static IEnumerable<Emp> Task1()
+    {
+        return Emps.Where(e => e.Job == "Backend programmer");
+    }
+
+    /// <summary>
+    ///     SELECT * FROM Emps Job = "Frontend programmer" AND Salary>1000 ORDER BY Ename DESC;
+    /// </summary>
+    public static IEnumerable<Emp> Task2()
+    {
+        return Emps.Where(e => e.Job == "Frontend programmer" && e.Salary > 1000)
+            .OrderByDescending(e => e.Ename);
+    }
+
+
+    /// <summary>
+    ///     SELECT MAX(Salary) FROM Emps;
+    /// </summary>
+    public static int Task3()
+    {
+        return Emps.Max(e => e.Salary);
+    }
+
+    /// <summary>
+    ///     SELECT * FROM Emps WHERE Salary=(SELECT MAX(Salary) FROM Emps);
+    /// </summary>
+    public static IEnumerable<Emp> Task4()
+    {
+        return Emps.Where(e => e.Salary == Emps.Max(emp => emp.Salary));
+    }
+
+    /// <summary>
+    ///    SELECT ename AS Nazwisko, job AS Praca FROM Emps;
+    /// </summary>
+    public static IEnumerable<object> Task5()
+    {
+        return Emps.Select(e => new { Nazwisko = e.Ename, Praca = e.Job });
+    }
+
+    /// <summary>
+    ///     SELECT Emps.Ename, Emps.Job, Depts.Dname FROM Emps
+    ///     INNER JOIN Depts ON Emps.Deptno=Depts.Deptno
+    ///     Rezultat: Złączenie kolekcji Emps i Depts.
+    /// </summary>
+    public static IEnumerable<object> Task6()
+    {
+        return Emps.Join(Depts, emp => emp.Deptno, dept => dept.Deptno, (emp, dept) => new { emp.Ename, emp.Job, dept.Dname });
+    }
+
+    /// <summary>
+    ///     SELECT Job AS Praca, COUNT(1) LiczbaPracownikow FROM Emps GROUP BY Job;
+    /// </summary>
+    public static IEnumerable<object> Task7()
+    {
+        return Emps.GroupBy(emp => emp.Job).Select(gr => new {Praca = gr.Key, LiczbaPracownikow = gr.Count()});
+    }
+
+    /// <summary>
+    ///     Zwróć wartość "true" jeśli choć jeden
+    ///     z elementów kolekcji pracuje jako "Backend programmer".
+    /// </summary>
+    public static bool Task8()
+    {
+        return Emps.Any(emp => emp.Job == "Backend programmer");
+    }
+
+    /// <summary>
+    ///     SELECT TOP 1 * FROM Emp WHERE Job="Frontend programmer"
+    ///     ORDER BY HireDate DESC;
+    /// </summary>
+    public static Emp Task9()
+    {
+        return Emps.Where(emp => emp.Job == "Frontend Programmer").OrderByDescending(emp => emp.HireDate).FirstOrDefault();
+    }
+
+    /// <summary>
+    ///     SELECT Ename, Job, Hiredate FROM Emps
+    ///     UNION
+    ///     SELECT "Brak wartości", null, null;
+    /// </summary>
+    public static IEnumerable<object> Task10()
+    {
+        return Emps.Select(emp => new { emp.Ename, emp.Job, emp.HireDate })
+            .Union(new[] { new { Ename = "Brak wartości", Job = (string)null, HireDate = (DateTime?)null } });
+    }
+
+    /// <summary>
+    ///     Wykorzystując LINQ pobierz pracowników podzielony na departamenty pamiętając, że:
+    ///     1. Interesują nas tylko departamenty z liczbą pracowników powyżej 1
+    ///     2. Chcemy zwrócić listę obiektów o następującej srukturze:
+    ///     [
+    ///     {name: "RESEARCH", numOfEmployees: 3},
+    ///     {name: "SALES", numOfEmployees: 5},
+    ///     ...
+    ///     ]
+    ///     3. Wykorzystaj typy anonimowe
+    /// </summary>
+    public static IEnumerable<object> Task11()
+    {
+        return Emps.Join(Depts, emp => emp.Deptno, dept => dept.Deptno,
+            ((emp, dept) => new { name = dept.Dname, numOfEmployees = Emps.Where(e => e.Deptno == dept.Deptno).Count() }));
+    }
+
+    /// <summary>
+    ///     Napisz własną metodę rozszerzeń, która pozwoli skompilować się poniższemu fragmentowi kodu.
+    ///     Metodę dodaj do klasy CustomExtensionMethods, która zdefiniowana jest poniżej.
+    ///     Metoda powinna zwrócić tylko tych pracowników, którzy mają min. 1 bezpośredniego podwładnego.
+    ///     Pracownicy powinny w ramach kolekcji być posortowani po nazwisku (rosnąco) i pensji (malejąco).
+    /// </summary>
+    public static IEnumerable<Emp> Task12()
+    {
+        IEnumerable<Emp> result = Emps.GetEmpsWithSubordinates();
+        
+        return result;
+    }
+
+    /// <summary>
+    ///     Poniższa metoda powinna zwracać pojedyczną liczbę int.
+    ///     Na wejściu przyjmujemy listę liczb całkowitych.
+    ///     Spróbuj z pomocą LINQ'a odnaleźć tę liczbę, które występuja w tablicy int'ów nieparzystą liczbę razy.
+    ///     Zakładamy, że zawsze będzie jedna taka liczba.
+    ///     Np: {1,1,1,1,1,1,10,1,1,1,1} => 10
+    /// </summary>
+    public static int Task13(int[] arr)
+    {
+        return arr.GroupBy(e => e).Where(x => x.Count() % 2 != 0).Select(x => x.Key).First();
+    }
+
+    /// <summary>
+    ///     Zwróć tylko te departamenty, które mają 5 pracowników lub nie mają pracowników w ogóle.
+    ///     Posortuj rezultat po nazwie departament rosnąco.
+    /// </summary>
+    public static IEnumerable<Dept> Task14()
+    {
+        return Depts
+            .GroupJoin(Emps, dept => dept.Deptno, emp => emp.Deptno, (dept, emps) => new { Department = dept, EmployeeCount = emps.Count() })
+            .Where(x => x.EmployeeCount == 0 || x.EmployeeCount == 5)
+            .OrderBy(x => x.Department.Dname)
+            .Select(x => x.Department);
+    }
+}
